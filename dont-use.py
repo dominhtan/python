f104b5b4af1750fbe777e352a74697d3b7946c4d
dominhtan <85058029+dominhtan@users.noreply.github.com>
2021-08-09 20:17:40 +0700

    Update and rename pthon to dont-use.py

---
+++ dont-use.py
@@ -0,0 +1,9 @@
+import re
+strA = "DoMinhTan "
+strB = "Macdsoghosdhe"
+strG = "ax maycvhjneqeq"
+strC = strA[None:None:3]
+strD = strB[0:None:len(strB) - 1]
+strF = strG[2:6]
+strE = strC + strD + strF.capitalize()
+print(strE)
